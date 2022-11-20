1e59016116efbfb9b382b4db238728dec04e6695
mily96 <34488304+mily96@users.noreply.github.com>
2022-11-20 15:33:59 -0300

    Issue #21 : Create Pregunta class
    Initial configuration in:
    - models.py
    - serializers.py
    - views.py
    - urls.py
    
    Register model in admin.py

--- server/tadp_api/tadp_api/urls.py
+++ server/tadp_api/tadp_api/urls.py
@@ -5,11 +5,11 @@ from rest_framework import routers
 from trivia import views
 
 router = routers.DefaultRouter()
-#router.register(r'questions', views.QuestionViewSet)
-router.register(r'examen', views.ExamenViewSet)
+#router.register(r'examen', views.ExamenViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('examen/', admin.site.urls) #URL de examen
+    path('examen/', include(router.urls)), #URL de examen
+    #path('',include(router.urls),name="Examen") #URL de examen
 ]
