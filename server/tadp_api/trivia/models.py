eb46c7210158e1914aa06757bf98477561aea5ba
mily96 <34488304+mily96@users.noreply.github.com>
2022-11-20 16:51:06 -0300

    Issue #19: Remove commented code

--- server/tadp_api/trivia/models.py
+++ server/tadp_api/trivia/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 
 
-#class Question(models.Model):
-#    category = models.CharField(max_length=50)
-#    question = models.TextField()
-
-#    def __str__(self):
-#        return self.question
-
-
-#class Answer(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-#    answer = models.TextField()
-#    is_correct = models.BooleanField()
-
-#    def __str__(self):
-#        return self.answer
 
 #Defino la clase Examen
 class Examen(models.Model):
