e0026a32308a02f898053e95de276352ae457723
mily96 <34488304+mily96@users.noreply.github.com>
2022-11-20 23:37:16 -0300

    Issue #21 feat: Create Pregunta class  (#25)
    * Issue #21 : Create Pregunta class
    
    Initial configuration in:
    - models.py
    - serializers.py
    - views.py
    - urls.py
    
    Register model in admin.py
    
    * Issue 21: Add Preguntas URL

--- server/tadp_api/trivia/models.py
+++ server/tadp_api/trivia/models.py
@@ -8,3 +8,12 @@ class Examen(models.Model):
     fecha = models.DateField(auto_now_add=True) #Guarda la fecha de hoy
     duracion = models.IntegerField()
     realizado = models.BooleanField()
+
+#Defino la clase Pregunta
+class Pregunta(models.Model):
+    id_pregunta = models.IntegerField(primary_key=True)
+    descripcion = models.TextField()
+    opcion_uno = models.CharField(max_length=100)
+    opcion_dos = models.CharField(max_length=100)
+    opcion_tres = models.CharField(max_length=100)
+    opcion_correcta = models.CharField(max_length=100)
--- server/tadp_api/trivia/views.py
+++ server/tadp_api/trivia/views.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets
 
-#from .models import Question, Examen
-from .models import Examen
-#from .serializers import QuestionSerializer
-from .serializers import ExamenSerializer
 
-#class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
-#    queryset = Question.objects.all()
-#    serializer_class = QuestionSerializer
+from .models import Examen, Pregunta
+from .serializers import ExamenSerializer, PreguntaSerializer
+
 
 class ExamenViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Examen.objects.all()
     serializer_class = ExamenSerializer
+
+class PreguntaViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Pregunta.objects.all()
+    serializer_class = PreguntaSerializer
