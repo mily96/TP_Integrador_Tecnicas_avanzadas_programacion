9ca0a8e4474c65b2790be5e2d50246a476aae0b4
Andrés Biso <biso.andres@gmail.com>
2022-11-21 13:27:20 -0300

    Add postgresql connection (#28)
    * feat: add psycopg2 to requirements.txt
    
    * TADP-6 feat: add connection with postgresql

--- server/tadp_api/tadp_api/settings.py
+++ server/tadp_api/tadp_api/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'tadp_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tadp', # mydatabase
+        'USER': 'postgres', # mydatabaseuser
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
