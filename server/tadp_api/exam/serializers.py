56d74babd6a148bffb3bd2d204a934a01634f492
Andres Biso <biso.andres@gmail.com>
2022-11-22 00:45:32 -0300

    TADP-31 feat: add model serializers, urls and views

--- server/tadp_api/exam/serializers.py
+++ server/tadp_api/exam/serializers.py
@@ -1,5 +1,21 @@
 from rest_framework import serializers
-from .models import (Opcion, Pregunta)
+from .models import (
+    Clave,
+    Examen,
+    Opcion,
+    Pregunta,
+    PreguntaOpcion,
+    Respuesta,
+    TurnoExamen,
+    TurnoRevision,
+    Usuario
+)
+
+class OpcionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Opcion 
+        fields = ['id_opcion', 'descripcion']
 
 class PreguntaSerializer(serializers.ModelSerializer):
 
@@ -7,9 +23,79 @@ class PreguntaSerializer(serializers.ModelSerializer):
         model = Pregunta 
         fields = ['id_pregunta', 'descripcion']
         
-        
-class OpcionSerializer(serializers.ModelSerializer):
+class UsuarioSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Opcion 
-        fields = ['id_opcion', 'descripcion']
+        model = Usuario 
+        fields = [
+            'id_usuario',
+            'tipo',
+            'habilitado',
+            'anteojos',
+            'nombre_usuario',
+            'contrasenia'
+        ]
+
+class ClaveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Clave 
+        fields = [
+            'id_clave',
+            'valor',
+            'fecha_creacion',
+            'es_valida'
+        ]
+        
+class TurnoExamenSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TurnoExamen 
+        fields = [
+            'id_turno_examen',
+            'id_usuario',
+            'fecha'
+        ]
+
+class TurnoRevisionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TurnoRevision 
+        fields = [
+            'id_turno_revision',
+            'id_usuario',
+            'fecha'
+        ]
+
+class ExamenSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Examen 
+        fields = [
+            'id_examen',
+            'id_usuario',
+            'id_clave',
+            'fecha',
+            'duracion',
+            'realizado'
+        ]
+        
+class PreguntaOpcionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PreguntaOpcion 
+        fields = [
+            'id_pregunta',
+            'id_opcion',
+            'opcion_correcta'
+        ]
+        
+class RespuestaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Respuesta 
+        fields = [
+            'id_examen',
+            'id_pregunta',
+            'id_opcion'
+        ]
